9b0a6c73baaa2025b09d8904f4a8e22e5f316e48
Solomiya Lisova <solomiyaya@icloud.com>
2021-02-09 23:52:38 +0200

    Made Output1

--- src/main/java/exceptions/Output1.java
+++ src/main/java/exceptions/Output1.java
@@ -1,8 +1,6 @@
 package exceptions;
 
-import java.sql.SQLOutput;
-
-public class App {
+public class Output1 {
 
 
     public static void main(String[] args) {
@@ -10,18 +8,27 @@ public class App {
         System.err.println("#1.in");
         try {
             f();
-
             g();
             h();
         }catch (Exception e){
-
-
-
+            System.err.println("#1.CATCH");
         }
-        //put code here
         finally {
             System.err.println("#1.out");
         }
+
+//        System.err.println("#1.in");
+//        try {
+//            f();
+//            g();
+//            h();
+//        }catch (RuntimeException e){
+//            System.err.print("RUNTIME EXCEPTION: ");
+//            e.printStackTrace();
+//        }
+
+
+
 
     }
 
@@ -30,7 +37,7 @@ public class App {
             System.err.println(".   #2.in");
             //throw new Exception();
         }catch(Exception e1){
-            System.out.println("CATCH");
+            System.err.println(".   #2.CATCH");
             System.err.println(".   #2.out");
         }
 
@@ -41,21 +48,21 @@ public class App {
             System.err.println(".   .   #3.in");
             //throw new Exception();
         }catch(Exception e2){
-            System.out.println("CATCH");
+            System.err.println(".   .   #3.CATCH");
             System.err.println(".   .   #3.out");
         }
 
     }
 
-    public static void h() {
-        try {
+    public static void h() throws RuntimeException{
+//        try {
             System.err.println(".   .   .   #4.in");
             System.err.println(".   .   .   #4.THROW");
-            //throw new Exception();
-        }catch (Exception e3) {
-            System.err.println(".   .   .   #4.out");
-        }
-
+            throw new RuntimeException();
+//        }catch (Exception e3) {
+//            System.err.println(".   .   .   #4.CATCH");
+//        }
+        //System.err.println(".   .   .   #4.out");
     }
 
 }
