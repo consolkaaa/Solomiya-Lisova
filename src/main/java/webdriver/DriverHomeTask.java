e51a7691a7fdefd441cca63d663e57371bb78fb7
Solomiya Lisova <solomiyaya@icloud.com>
2021-02-20 12:46:47 +0200

    added to cart and proceeded to checkout

--- src/main/java/webdriver/DriverHomeTask.java
+++ src/main/java/webdriver/DriverHomeTask.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -12,7 +14,8 @@ public class DriverHomeTask {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        WebElement searchInput, searchButton, dress;
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement searchInput, searchButton, dress, checkout;
 
         driver.get("http://automationpractice.com/index.php");
         searchInput = driver.findElement(By.id("search_query_top"));
@@ -21,7 +24,13 @@ public class DriverHomeTask {
         searchButton = driver.findElement(By.name("submit_search"));
         searchButton.click();
 
-        driver.quit();
+        dress = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(), 'Faded')]" +
+                "/ancestor::div[@class = 'right-block']/div[@class = 'button-container']")));
+        dress.click();
 
+        checkout = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@title='Proceed to checkout']")));
+        checkout.click();
+
+        driver.quit();
     }
 }
