347884ddc73ed19f9e699249d8a8f4858f359cda
Solomiya Lisova <solomiyaya@icloud.com>
2021-02-21 18:49:17 +0200

    added tests

--- src/test/java/testng/WebStoreTest.java
+++ src/test/java/testng/WebStoreTest.java
@@ -9,19 +9,22 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.testng.Assert.assertTrue;
 
 public class WebStoreTest {
     WebDriver driver;
 
-    @BeforeTest
+    @BeforeClass
     public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("http://automationpractice.com/index.php");
     }
 
-    @AfterTest
+    @AfterClass
     public void teardown(){
         driver.quit();
     }
@@ -31,18 +34,34 @@ public class WebStoreTest {
        assertTrue(driver.getTitle().equals("My Store"));
     }
 
-    @Test
-    public void addToCartTest(){
-        WebElement searchInput, searchButton;
+    @Test(dependsOnMethods = "titleTest")
+    public void checkPrice(){
+        WebElement dress;
 
-        searchInput = driver.findElement(By.id("search_query_top"));
-        searchInput.sendKeys("dress");
+        List<WebElement> elementList = driver.findElements(By.xpath("//li[contains(@class, 'ajax_block_product')]"));
+        dress = elementList.stream().filter(element -> element.getText().contains("Chiffon")).findAny().orElseThrow();
 
-        searchButton = driver.findElement(By.name("submit_search"));
-        searchButton.click();
-
-        //assertTrue();
+        assertTrue(dress.getText().contains("$16.40"));
     }
 
+    @Test(dependsOnMethods = {"checkPrice","titleTest"})
+    public void addToCartTest(){
+        WebElement dressBox, addToCart, checkout, dress;
+
+        dressBox = driver.findElement(By.xpath("//a[contains(text(), 'Chiffon')]/ancestor::div[@class = 'right-block']"));
+        dressBox.click();
+
+        addToCart = driver.findElement(By.xpath("//a[@title = 'Add to cart' and @data-id-product = '7']"));
+        addToCart.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        checkout = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@title='Proceed to checkout']")));
+        checkout.click();
+
+        List<WebElement> elementList = driver.findElements(By.xpath("//tr[contains(@class,'cart_item')]"));
+        dress = elementList.stream().filter(element -> element.getText().contains("Chiffon")).findAny().orElseThrow();
+
+        assert dress != null;
+    }
 
 }
