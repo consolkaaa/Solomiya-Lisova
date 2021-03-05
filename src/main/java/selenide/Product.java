52e82343edb99c3f7dd367e925b5646eee6c0dd0
Solomiya Lisova <solomiyaya@icloud.com>
2021-03-05 09:05:22 +0200

    added Product

---
+++ src/main/java/selenide/Product.java
@@ -0,0 +1,13 @@
+package selenide;
+
+public class Product {
+    private String name;
+    private String price;
+
+    public Product(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+}
